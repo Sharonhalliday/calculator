173cb4e1efe23277f0804ddcec8f844ac94450cc
sharonhalliday <hallidaysharon5@gmail.com>
2023-11-21 14:21:52 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//function to add
+function add(number1, number2) {
+    return number1 + number2;
+}
+//modulus function
+function modulus(number1,number2){
+    return number1 % number2;
+}
+//function to subtract
+function subtract(number1, number2){
+    return number1 - number2;
+}
+//function to multiply
+function multiply(a,b){
+    return a * b;
+}
+//fucntion to divide 
+function divide(num1,num2){
+    return num1 / num2;
+}   
+
+alert(divide(50, 5));
+
